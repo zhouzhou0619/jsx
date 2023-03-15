21685d4b8e72323b545189f6ea019107e3362354
Yihan Zhou <yzho0181@student.monash.edu>
2023-03-15 20:34:47 +1100

    15/03/2023night

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 // 1) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App'
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el); 
-// 4) Create a component
-function App() {
-    return <input placeholder="hi there" />
-    // return <input style={{border: '1px solid blue'}} />;
-}
-// 5) Show the component on the screen
+
+// 4) Show the component on the screen
 root.render(<App />);
